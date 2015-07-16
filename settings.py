c08d6aa6452b6c29d338349b0a1d0cd538b74a30
falconpy <falconp@gmail.com>
2015-07-16 16:33:19 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings(object):
+    showrssURL = 'http://showrss.info/rss.php?user_id=XXXXXX'
+    dir_download = '/home/user/downloads/watch'
+    log_enabled = True
+    space_alert = 2.0
+    email_enabled = True
+    email_user = 'sender_email@gmail.com'
+    email_pwd = 'sender_email_pass'
+    email_dst = {'email1@gmail.com','email2@hotmail.com'}
